be1fc80f0949d07fc24e3f02f710c3e3332c4a7b
IbrahimELE <118020507+IbrahimELE@users.noreply.github.com>
2024-11-04 22:46:31 +0100

    Transitioning to master (#3)
    * Commit 2
    
    * Updated structure, fixed register, added .env files
    
    * Functional register, login, update task, delete etc...
    
    * added requirements.txt
    
    * added sort tasks by priority, status and deadline
    
    * security fixes
    
    ---------
    
    Co-authored-by: Ibrahim <ibrahimibrisimovic7@gmail.com>

---
+++ app/crud/task.py
@@ -0,0 +1,54 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import desc, asc
+from models.task import Task
+from schemas.task import TaskCreate
+
+
+def create_task(db: Session, task: TaskCreate, user_id: str):
+    db_task = Task(
+        title = task.title,
+        content = task.content,
+        deadline = task.deadline,
+        priority = task.priority,
+        status = task.status,
+        user_id=user_id 
+        )
+    
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+
+def get_tasks(db: Session, user_id: str):
+    return db.query(Task).filter(Task.user_id == user_id).all()
+
+def get_task(db: Session, task_id: int, user_id: str):
+    return db.query(Task).filter(Task.id == task_id).first()
+
+def get_tasks_by_priority(db: Session, user_id: str):
+    return db.query(Task).filter(Task.user_id == user_id).order_by(desc(Task.priority)).all()
+
+def get_tasks_by_status(db: Session, user_id: str, status: str):
+    return db.query(Task).filter(Task.user_id==user_id, Task.status == status).all()
+
+def get_task_by_deadline(db: Session, user_id: str):
+    return db.query(Task).filter(Task.user_id == user_id).order_by(asc(Task.deadline)).all()
+
+def update_task(db: Session, task_id: int, task: TaskCreate):
+    db_task = db.query(Task).filter(Task.id == task_id).first()
+    if db_task:
+        for key, value in task.model_dump().items():
+            setattr(db_task, key, value)
+        db.commit()
+        db.refresh(db_task)
+        return db_task
+    return None
+
+def delete_task(db: Session, task_id: int):
+    db_task = db.query(Task).filter(Task.id == task_id).first()
+    if db_task:
+        db.delete(db_task)
+        db.commit()
+        return db_task
+    return None
--- app/crud/user.py
+++ app/crud/user.py
@@ -1,12 +1,13 @@
 from sqlalchemy.orm import Session
-from app.models import UserDB
-from app.schemas import UserCreate
+from models.user import UserDB
+from schemas.user import UserCreate
 import bcrypt
 
 def create_user(db: Session, user: UserCreate):
     hashed_password = bcrypt.hashpw(user.password.encode("utf-8"), bcrypt.gensalt())
+    
     db_user = UserDB(
-        e_mail=user.e_mail,
+        email_address=user.email_address,
         name=user.name,
         password=hashed_password.decode("utf-8"),
         role=user.role,
@@ -16,5 +17,7 @@ def create_user(db: Session, user: UserCreate):
     db.refresh(db_user)
     return db_user
 
-def get_user_by_email(db: Session, e_mail: str):
-    return db.query(UserDB).filter(UserDB.e_mail == e_mail).first()
+def get_user_by_email(db: Session, email_address: str):
+    return db.query(UserDB).filter(UserDB.email_address == email_address).first()
+
+
